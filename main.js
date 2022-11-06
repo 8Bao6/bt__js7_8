2b5dc11e79fe388a45f89b446856cc1bfeff5fd7
8Bao6 <bao8696@gmail.com>
2022-11-07 00:18:29 +0700

    bt

---
+++ main.js
@@ -0,0 +1,47 @@
+listNumber = [];
+minDuong = [];
+function addNumber() {
+  var number = document.getElementById("txtNumber").value * 1;
+
+  listNumber.push(number);
+
+  document.getElementById("infoListNumber").innerHTML = listNumber;
+}
+
+
+function tinhTongSoDuong() {
+  var soDuong = 0;
+  for (var i = 0; i < listNumber.length; i++) {
+    if (listNumber[i] > 0) {
+      soDuong += listNumber[i];
+    }
+  }
+  var result = "Tổng các số dương là: " + soDuong;
+  document.getElementById("infoTongDuong").innerHTML = result;
+}
+
+function demSoDuong() {
+  var dem = 0;
+  for (var i = 0; i < listNumber.length; i++) {
+    if (listNumber[i] > 0) {
+      dem++;
+    }
+  }
+  var result = "Có tất cả: " + dem + " Số Dương";
+  document.getElementById("infoDemSoDuong").innerHTML = result;
+}
+
+function soNhoNhat() {
+  var min = listNumber[0]
+  for (var i = 1; i < listNumber.length; i++) {
+    if (min > listNumber[i]) {
+      min = listNumber[i]
+    }
+  }
+  var result = "Số nhỏ nhất là: " + min;
+  document.getElementById("infoSoNhoNhat").innerHTML = result;
+}
+
+
+
+
